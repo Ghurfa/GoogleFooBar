efccacba6945e9ae47b4e457e2ff18fa0e12c523
Lorenzo Lopez <geltbm@gmail.com>
2021-11-04 21:33:05 -0700

    Foobar 7: Changed the count pairings method from the brute force solution to a node-picking solution. Still slow

--- Foobar7.java
+++ Foobar7.java
@@ -1,57 +1,33 @@
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Foobar7 {
-    private static class Pairing {
-        public int indexA;
-        public int indexB;
-        
-        public Pairing(int a, int b) {
-            indexA = a;
-            indexB = b;
-        }
-    }
+    private static class GuardNode {
+        public Set<GuardNode> neighbors = new HashSet();
+        public boolean isUsed = false;
+        public int value; //for debugging purposes
 
-    private static boolean doesGameEndOld(int startA, int startB) {
-        int slowA = startA;
-        int slowB = startB;
-        int fastA = startA;
-        int fastB = startB;
-        if(fastA > fastB) {
-            fastA -= fastB;
-            fastB *= 2;
+        public void addNeighbor(GuardNode node) {
+            neighbors.add(node);
         }
-        else {
-            fastB -= fastA;
-            fastA *= 2;
+
+        public int degree() {
+            return neighbors.size();
         }
-        while(slowA != slowB && slowA != fastA) {
-            if(slowA > slowB) {
-                slowA -= slowB;
-                slowB *= 2;
+
+        public void disable() {
+            for(GuardNode neighbor : neighbors) {
+                neighbor.neighbors.remove(this);
             }
-            else {
-                slowB -= slowA;
-                slowA *= 2;
-            }
-            if(fastA > fastB) {
-                fastA -= fastB;
-                fastB *= 2;
-            }
-            else {
-                fastB -= fastA;
-                fastA *= 2;
-            }
-            if(fastA > fastB) {
-                fastA -= fastB;
-                fastB *= 2;
-            }
-            else {
-                fastB -= fastA;
-                fastA *= 2;
-            }
+            isUsed = true;
         }
-        return slowA == slowB;
+
+        public void enable() {
+            for(GuardNode neighbor : neighbors) {
+                neighbor.neighbors.add(this);
+            }
+            isUsed = false;
+        }
     }
 
     private static boolean doesGameEnd(int startA, int startB) {
@@ -79,55 +55,72 @@ public class Foobar7 {
         return true;
     }
 
-    private static int getMostPairings(ArrayList<Pairing> pairings, boolean[] usedGuards, int startAt) {
-        int best = 0;
-        for(int i = startAt; i < pairings.size() - best; i++) {
-            Pairing pair = pairings.get(i);
-            if(!usedGuards[pair.indexA] && !usedGuards[pair.indexB]) {
-                usedGuards[pair.indexA] = true;
-                usedGuards[pair.indexB] = true;
-                int score = getMostPairings(pairings, usedGuards, i + 1) + 1;
-                if(best < score) {
-                    best = score;
-                }
-                usedGuards[pair.indexA] = false;
-                usedGuards[pair.indexB] = false;
+    private static int selectLowestDegree(GuardNode[] nodes) {
+        int lowestDeg = Integer.MAX_VALUE;
+        int lowestDegIndex = -1;
+        for(int i = 0; i < nodes.length; i++) {
+            if(!nodes[i].isUsed && nodes[i].degree() > 0 && nodes[i].degree() < lowestDeg) {
+                lowestDegIndex = i;
             }
         }
-        return best;
+        return lowestDegIndex;
+    }
+
+    private static int countMostPairings(GuardNode[] nodes) {
+        int lowestDegIndex = selectLowestDegree(nodes);
+        if(lowestDegIndex >= 0) {
+            GuardNode loneliest = nodes[lowestDegIndex];
+            loneliest.disable();
+
+            int bestScore = 0;
+            for(GuardNode neighbor : loneliest.neighbors) {
+                if(neighbor.isUsed) {
+                    throw new RuntimeException("shouldn't happen?");
+                }
+                neighbor.disable();
+                int score = countMostPairings(nodes) + 1;
+                if(score > bestScore) {
+                    bestScore = score;
+                }
+                neighbor.enable();
+            }
+
+            loneliest.enable();
+            return bestScore;
+        }
+        else return 0;
     }
 
     public static int solution(int[] banana_list) {
-        ArrayList<Pairing> infinitePairings = new ArrayList();
+        GuardNode[] nodes = new GuardNode[banana_list.length];
 
+        for(int i = 0; i < nodes.length; i++) {
+            nodes[i] = new GuardNode();
+            nodes[i].value = banana_list[i];
+        }
+
+        //Build graph
         for(int i = 0; i < banana_list.length - 1; i++) {
             int countA = banana_list[i];
             for(int j = i + 1; j < banana_list.length; j++) {
                 int countB = banana_list[j];    
                 boolean ends = doesGameEnd(countA, countB);
-                boolean endsOld = doesGameEndOld(countA, countB);
-                if(ends != endsOld) {
-                    int a = 2;
-                }
                 if(!ends) {
-                    infinitePairings.add(new Pairing(i, j));
+                    nodes[i].addNeighbor(nodes[j]);
+                    nodes[j].addNeighbor(nodes[i]);
                 }
             }
         }
 
-        for(Pairing pair : infinitePairings) {
-            System.out.println(pair.indexA + "\t" + pair.indexB);
-        }
-
-        boolean[] usedGuards = new boolean[banana_list.length];
-        return banana_list.length - 2 * getMostPairings(infinitePairings, usedGuards, 0);
+        return banana_list.length - 2 * countMostPairings(nodes);
     }
 
     public static void main(String[] args) {
-        int[] bananaList = new int[9];
-        for(int i = 0; i < 9; i++) {
+        int[] bananaList = new int[17];
+        for(int i = 0; i < 17; i++) {
             bananaList[i] = i;
         }
+        //int[] bananaList = {16, 2, 6, 1};
         System.out.println(solution(bananaList));
     }
 }
