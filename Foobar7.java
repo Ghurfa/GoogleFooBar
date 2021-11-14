cfae27c13fa5037f5beb44d618955ecda1b43244
Lorenzo Lopez <geltbm@gmail.com>
2021-11-14 13:08:05 -0800

    Completed foobar 7 with a solution that focused on grouping the numbers modulo 4

--- Foobar7.java
+++ Foobar7.java
@@ -1,162 +1,260 @@
-import java.util.Set;
-import java.util.HashSet;
-import java.util.TreeSet;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.List;
 
 public class Foobar7 {
-    private static class GuardNode implements Comparable{
-        public Set<GuardNode> neighbors = new TreeSet();
-        public boolean isUsed = false;
-        public int value; //for debugging purposes
+    private static class OddsList implements Iterable<Integer> {
+        private List<Integer> sortedByCountDesc;
+        private Map<Integer, Integer> freqMap;
+        private int size;
 
-        public int compareTo(Object other) {
-            return value - ((GuardNode)other).value;
-        }
-
-        public void addNeighbor(GuardNode node) {
-            neighbors.add(node);
-        }
-
-        public int degree() {
-            return neighbors.size();
-        }
-
-        public void disable() {
-            for(GuardNode neighbor : neighbors) {
-                neighbor.neighbors.remove(this);
+        public OddsList(Map<Integer, Integer> frequencyList) {
+            freqMap = frequencyList;
+            size = 0;
+            sortedByCountDesc = new ArrayList<>();
+            for (int num : frequencyList.keySet()) {
+                sortedByCountDesc.add(num);
+                size += freqMap.get(num);
             }
-            isUsed = true;
+            Collections.sort(sortedByCountDesc, (x, y) -> freqMap.get(y) - freqMap.get(x));
         }
 
-        public void enable() {
-            for(GuardNode neighbor : neighbors) {
-                neighbor.neighbors.add(this);
+        public void useValue(int val) {
+            int newFreq = freqMap.get(val) - 1;
+            freqMap.put(val, newFreq);
+
+            if (newFreq == 0) {
+                sortedByCountDesc.remove(val);
+            } else {
+                Collections.sort(sortedByCountDesc, (x, y) -> freqMap.get(y) - freqMap.get(x));
             }
-            isUsed = false;
+            size--;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        // The dominator is the value which comprises more than half the list
+        // Returns null if there isn't one
+        public Integer getDominator() {
+            Integer val = sortedByCountDesc.get(0);
+            if (freqMap.get(val) * 2 > size) {
+                return val;
+            } else {
+                return null;
+            }
+        }
+
+        public int getDominationAmount() {
+            return 2 * freqMap.get(sortedByCountDesc.get(0)) - size;
+        }
+
+        @Override
+        public Iterator<Integer> iterator() {
+            return sortedByCountDesc.iterator();
+        }
+    }
+
+    private static void sortMod2(List<Integer> list, List<Integer> evens, List<Integer> odds) {
+        for (int num : list) {
+            if (num % 2 == 0) {
+                evens.add(num);
+            } else {
+                odds.add(num);
+            }
+        }
+    }
+
+    private static void addToFreqMap(Map<Integer, Integer> map, Integer val) {
+        Integer oldCount = map.get(val);
+
+        if (oldCount == null) {
+            map.put(val, 1);
+        } else {
+            map.put(val, oldCount + 1);
+        }
+    }
+
+    private static void sortOddsMod4(List<Integer> odds, Map<Integer, Integer> con1, Map<Integer, Integer> con3) {
+        for (int num : odds) {
+            if (num % 4 == 1) {
+                addToFreqMap(con1, num);
+            } else {
+                addToFreqMap(con3, num);
+            }
         }
     }
 
     private static boolean doesGameEnd(int startA, int startB) {
         int currA = startA;
         int currB = startB;
-        while(currA != currB) {
-            if(currA % 2 != currB % 2) {
+        while (currA != currB) {
+            if (currA % 2 != currB % 2) {   // Odd & even -> infinite
                 return false;
-            }
-            else if(currA % 2 == 0) {
+            } else if (currA % 2 == 0) {    // Both even -> reduce
                 currA /= 2;
                 currB /= 2;
-            }
-            else { //Both are odd
-                if(currA < currB) {
-                    currB -= currA;
-                    currA *= 2;
-                }
-                else {
-                    currA -= currB;
-                    currB *= 2;
+            } else {                        // Both odd -> play a round & reduce
+                if (currA < currB) {
+                    currB = (currB - currA) / 2;
+                } else {
+                    currA -= (currA - currB) / 2;
                 }
             }
         }
         return true;
     }
 
-    private static int selectLowestDegree(GuardNode[] nodes) {
-        int lowestDeg = Integer.MAX_VALUE;
-        int lowestDegIndex = -1;
-        for(int i = 0; i < nodes.length; i++) {
-            if(!nodes[i].isUsed && nodes[i].degree() > 0 && nodes[i].degree() < lowestDeg) {
-                lowestDegIndex = i;
-            }
-        }
-        return lowestDegIndex;
-    }
-
-    private static int countMostPairings(GuardNode[] nodes) {
-        int lowestDegIndex = selectLowestDegree(nodes);
-        if(lowestDegIndex >= 0) {
-            GuardNode loneliest = nodes[lowestDegIndex];
-            loneliest.disable();
-
-            int bestScore = 0;
-            for(GuardNode neighbor : loneliest.neighbors) {
-                if(neighbor.isUsed) {
-                    throw new RuntimeException("shouldn't happen?");
+    private static boolean findPairing(Iterable<Integer> listA, Iterable<Integer> listB) {
+        for (int a : listA) {
+            for (int b : listB) {
+                if (!doesGameEnd(a, b)) {
+                    return true;
                 }
-                neighbor.disable();
-                int score = countMostPairings(nodes) + 1;
-                if(score > bestScore) {
-                    bestScore = score;
-                }
-                neighbor.enable();
-            }
-
-            loneliest.enable();
-            return bestScore;
-        }
-        else return 0;
-    }
-
-    private static <E> boolean hasIntersection(Set<E> a, Set<E> b) {
-        for(E item : a) {
-            if(b.contains(item)) {
-                return true;
             }
         }
         return false;
     }
 
+    private static int solveOdds(List<Integer> odds) {
+        Map<Integer, Integer> con1Freq = new HashMap<>(); // Frequency map of values that are congruent to 1 (mod 4)
+        Map<Integer, Integer> con3Freq = new HashMap<>();
+        sortOddsMod4(odds, con1Freq, con3Freq);
+        OddsList con1 = new OddsList(con1Freq); // Nums that are congruent (mod 4) can be paired w/ each other
+        OddsList con3 = new OddsList(con3Freq);
+
+        while (con1.size() > 0 && con3.size() > 0) {
+            /*
+             * Dominator = value that comprises more than half its list. It cannot be paired
+             * within its list and should be paired w/ the other list
+             */
+            Integer con1Dom = con1.getDominator();
+            Integer con3Dom = con3.getDominator();
+            if (con1Dom == null && con3Dom == null) {
+                // No dominators
+                if ((con1.size() % 2) == 0 && (con3.size % 2) == 0) {
+                    // Both lists can be paired within themselves perfectly
+                    return 0;
+                } else if ((con1.size() % 2) != (con3.size() % 2)) {
+                    // Both lists can be paired within themselves with one extra from one list
+                    return 1;
+                } else {
+                    // One extra from each list, might be able to be paired together
+                    return findPairing(con1, con3) ? 0 : 2;
+                }
+            } else if (con1Dom != null && con3Dom != null) {
+                // Try to pair the dominators of the two lists
+                int con1DomAmt = con1.getDominationAmount();
+                int con3DomAmt = con3.getDominationAmount();
+                if (doesGameEnd(con1Dom, con3Dom)) {
+                    return con3DomAmt > con1DomAmt ? con3DomAmt - con1DomAmt : con1DomAmt - con3DomAmt;
+                } else {
+                    return con1DomAmt + con3DomAmt;
+                }
+            } else {
+                // Pair the dominator of one list with any value in the other
+                OddsList hasDom;
+                OddsList hasntDom;
+                int dom;
+                if (con1Dom == null) {
+                    hasDom = con3;
+                    hasntDom = con1;
+                    dom = con3Dom;
+                } else {
+                    hasDom = con1;
+                    hasntDom = con3;
+                    dom = con1Dom;
+                }
+
+                boolean foundPair = false;
+                for (Integer val : hasntDom) {
+                    if (!doesGameEnd(dom, val)) {
+                        hasDom.useValue(dom);
+                        hasntDom.useValue(val);
+                        foundPair = true;
+                        break;
+                    }
+                }
+                if (!foundPair) {
+                    // Dominator cannot be paired w/ any in the other list
+                    return hasDom.getDominationAmount() + (hasntDom.size() % 2 == 0 ? 0 : 1);
+                }
+            }
+        }
+
+        OddsList nonEmpty;
+        if (con1.size() == 0) {
+            if (con3.size() == 0) {
+                return 0;
+            }
+            nonEmpty = con3;
+        } else {
+            nonEmpty = con1;
+        }
+
+        // Make pairs within the non-empty list
+        Integer dominator = nonEmpty.getDominator();
+        if (dominator == null) {
+            return nonEmpty.size() % 2 == 0 ? 0 : 1;
+        } else {
+            return nonEmpty.getDominationAmount();
+        }
+    }
+
+    private static int solveEvensOdds(List<Integer> list) {
+        List<Integer> evens = new LinkedList<>();
+        List<Integer> odds = new LinkedList<>();
+        sortMod2(list, evens, odds);
+
+        int diff = evens.size() - odds.size();
+
+        if (diff == 0) { // Evens can be paired w/ odds perfectly
+            return 0;
+        } else if (diff == 1 || diff == -1) { // Evens can be paired w/ odds w/ one extra
+            return 1;
+        } else if (diff > 1) { // Extra evens; try to pair evens w/ other evens
+            List<Integer> halved = new LinkedList<>();
+            for (int num : evens) {
+                halved.add(num / 2);
+            }
+            int evensRemaining = solveEvensOdds(halved);
+            int evensPaired = evens.size() - evensRemaining;
+            if (evensPaired > diff) {
+                return diff % 2 == 0 ? 0 : 1;
+            } else {
+                return diff - evensPaired;
+            }
+        } else { // Extra odds; try to pair odds w/ other odds
+            int oddsRemaining = solveOdds(odds);
+            int oddsPaired = odds.size() - oddsRemaining;
+            if (oddsPaired > -diff) {
+                return diff % 2 == 0 ? 0 : 1;
+            } else {
+                return -diff - oddsPaired;
+            }
+        }
+    }
+
     public static int solution(int[] banana_list) {
-        GuardNode[] nodes = new GuardNode[banana_list.length];
-
-        for(int i = 0; i < nodes.length; i++) {
-            nodes[i] = new GuardNode();
-            nodes[i].value = banana_list[i];
+        List<Integer> list = new LinkedList<Integer>();
+        for (int num : banana_list) {
+            list.add(num);
         }
-
-        //Build graph
-        for(int i = 0; i < banana_list.length - 1; i++) {
-            int countA = banana_list[i];
-            for(int j = i + 1; j < banana_list.length; j++) {
-                int countB = banana_list[j];    
-                boolean ends = doesGameEnd(countA, countB);
-                if(!ends) {
-                    nodes[i].addNeighbor(nodes[j]);
-                    nodes[j].addNeighbor(nodes[i]);
-                }
-            }
-        }
-        
-        String out = "";
-        for(int i = 0; i < nodes.length; i++) {
-            GuardNode node = nodes[i];
-            String print = "";
-            for(int j = 0; j < nodes.length; j++) {
-                GuardNode neighbor = nodes[j];
-                if(node.neighbors.contains(neighbor)) {
-                    print += "(" + node.value + ", " + neighbor.value+")" + ", ";
-                }
-            }
-            if(print.length() > 0) {
-                out += print.substring(0, print.length() - 2) + "\n";
-            }
-        }
-
-        StringSelection selection = new StringSelection(out);
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        clipboard.setContents(selection, selection); //paste into desmos
-
-        return 1;
+        return solveEvensOdds(list);
     }
 
     public static void main(String[] args) {
-        int[] bananaList = new int[30];
-        for(int i = 0; i < bananaList.length; i++) {
-            bananaList[i] = i;
+        int[] bananaList = { 1, 1 };
+        // int[] bananaList = { 1, 7, 3, 13, 19 };
+        int solution = solution(bananaList);
+        System.out.println(solution);
+        if (solution != Foobar7Old.solution(bananaList)) {
+            throw new RuntimeException();
         }
-        //int[] bananaList = {1, 3, 2, 6};
-        solution(bananaList);
     }
 }
